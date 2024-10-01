5e287b6e82eb71c549882e1f2e5303845e936c16
Adarsh-UV <adarshuv2028@cs.sjcetpalai.ac.in>
2024-10-01 15:23:18 +0530

    Add files via upload

--- Students_detail.py
+++ Students_detail.py
@@ -1,3 +1,8 @@
+'''
+python program to get the student details
+Author: Adarsh uv
+date 1-10-2024
+'''
 Name=input("enter the student name")
 Roll_no=int(input("enter the student name roll no"))
 Roll_no+=1
