90fd31b3f8557993357d140f69db70e14ff273cb
Adarsh-UV <adarshuv2028@cs.sjcetpalai.ac.in>
2024-10-15 14:41:53 +0530

    Add files via upload

--- adarshuv234.py
+++ adarshuv234.py
@@ -1,5 +1,7 @@
 number=int(input("Enter a number"))
 if number>0:
     print("The given number:", number," is positive")
-else:
+elif number<0:
     print("The given number:",number," is negative")
+else:
+    print("The given numaber:",number, "is zero")
